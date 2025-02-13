0fb074184b22461025c53ddd3c2a30eb8dac08fc
Zayf24 <shaikh.huzayfh@gmail.com>
2025-02-14 00:08:58 +0530

    'hi'

--- ChatApp/chatApplication/models.py
+++ ChatApp/chatApplication/models.py
@@ -1,6 +1,18 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 class Room(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender.username}: {self.message}'
